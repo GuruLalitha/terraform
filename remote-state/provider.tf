51f7c240aa3115dd45b62942f91898d9a7b43607
GuruLalitha <guru.lalitha90@gmail.com>
2025-01-31 18:37:57 +0530

    terraform

---
+++ remote-state/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+  bucket = "lalitha-remote-state"
+  key = "expense-backend-infra"
+  region = "us-east-1"
+  dynamodb_table = "guru-state-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
